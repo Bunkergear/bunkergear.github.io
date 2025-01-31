5c6ba7bce35f46aec1d553d6c30534f630f51c8f
Bunkergear <bunker.depmercadeo@gmail.com>
2025-01-31 15:57:42 -0400

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,29 @@
 /*=============== SHOW MENU ===============*/
 const showMenu = (toggleId, navId) =>{
-   const toggle = document.getElementById(toggleId),
-         nav = document.getElementById(navId)
+    const toggle = document.getElementById(toggleId),
+          nav = document.getElementById(navId)
+ 
+    toggle.addEventListener('click', () =>{
+        // Add show-menu class to nav menu
+        nav.classList.toggle('show-menu')
+ 
+        // Add show-icon to show and hide the menu icon
+        toggle.classList.toggle('show-icon')
+    })
+ }
+ 
+ showMenu('nav-toggle','nav-menu')
 
-   toggle.addEventListener('click', () =>{
-       // Add show-menu class to nav menu
-       nav.classList.toggle('show-menu')
-
-       // Add show-icon to show and hide the menu icon
-       toggle.classList.toggle('show-icon')
-   })
-}
-
-showMenu('nav-toggle','nav-menu')
+// Script para animar las secciones al hacer scroll
+window.addEventListener('scroll', function() {
+    const sections = document.querySelectorAll('.footer-section');
+    sections.forEach((section) => {
+      const position = section.getBoundingClientRect();
+      if (position.top >= 0 && position.bottom <= window.innerHeight) {
+        section.classList.add('animate');
+      } else {
+        section.classList.remove('animate');
+      }
+    });
+  });
+  
